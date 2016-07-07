9bfbc6459fed83d8be015c2f89f2b5f5dc0e371d
Matthew Cox <matthewcpcox@gmail.com>
2016-07-07 12:40:29 +0100

    Add genStats.sh script to split logs and regen stats page in one command

---
+++ genStats.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+home=/home/tyranic-moron
+
+echo "### Splitting single .weechatlog into daily logs..."
+python2 $home/stats_gen/sss_desertbus/splitLogs.py -f $home/.weechat/logs/irc.dbchat.\#desertbus.weechatlog -p $home/stats_gen/working_logs/newserver.desertbus. -s .weechatlog
+
+echo "### Importing daily logs and generating stats page..."
+php -d memory_limit=512M $home/stats_gen/superseriousstats/sss.php -c $home/stats_gen/sss_desertbus/desertbus.conf -i $home/stats_gen/working_logs -o $home/public_html/sss/index.html
+
+echo "### Done!"
