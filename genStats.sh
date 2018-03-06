589934738c62d7efbc24799af717960122852f2d
Matthew Cox <679547+MatthewCox@users.noreply.github.com>
2018-03-06 16:27:07 +0000

    redirect stats gen output to log file, add dates to logrotate conf

--- genStats.sh
+++ genStats.sh
@@ -8,6 +8,6 @@ python2 $home/stats_gen/sss_desertbus/splitLogs.py -f $home/.weechat/logs/irc.db
 #gzip $home/stats_gen/working_logs/*.weechatlog
 
 echo "### Importing daily logs and generating stats page..."
-php -d memory_limit=512M $home/stats_gen/superseriousstats/sss.php -c $home/stats_gen/sss_desertbus/desertbus.conf -i $home/stats_gen/working_logs -o $home/public_html/sss/index.html
+php -d memory_limit=512M $home/stats_gen/superseriousstats/sss.php -c $home/stats_gen/sss_desertbus/desertbus.conf -i $home/stats_gen/working_logs -o $home/public_html/sss/index.html &>> $home/stats_gen/sss_desertbus/sss.log
 
 echo "### Done!"
